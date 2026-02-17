70cfae5ef00316883d8925d3827eea0f7e39a4a8
vikramgaming <pikrampikram25@gmail.com>
2026-02-17 09:48:04 +0700

    fix parameter folder

--- src/tsconfig.ts
+++ src/tsconfig.ts
@@ -1,10 +1,16 @@
-import { writeFile } from "fs";
+import { writeFile, existsSync, mkdirSync } from "fs";
 import { cwd } from "process";
 import type { TSConfig } from "./tsConfigType.ts";
 
 export function tsc(config: TSConfig, inFolder = ""): void {
     if (inFolder && !inFolder.endsWith("/")) inFolder += "/";
-    writeFile(cwd() + "/" + inFolder + "tsconfig.json", JSON.stringify(config, null, 4), (err) => {
+    const path = cwd() + "/" + inFolder;
+    
+    if (!existsSync(path)) {
+        mkdirSync(path, { recursive: true });
+    }
+    
+    writeFile(path + "tsconfig.json", JSON.stringify(config, null, 4), (err) => {
         if (err) console.log(err);
     });
 }
